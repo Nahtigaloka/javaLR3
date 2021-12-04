2c519c42ad65c8bdfd6fc0fbd357f46643e5388f
Nahtigaloka <92973928+Nahtigaloka@users.noreply.github.com>
2021-12-04 11:56:02 +0300

    Update JavaLR3.java

--- JavaLR3.java
+++ JavaLR3.java
@@ -2,102 +2,87 @@ package javalr3;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
-
+import java.util.List;
+import java.util.Random;
 
 public class JavaLR3 {
 
+    public static final Random random = new Random();
+    
     public static void main(String[] args) {
-        int N=20000;
-        LinkedList<Integer> linkedList = new LinkedList<>();
-        ArrayList<Integer> arrayList = new ArrayList<>();
+        int N=50000;
+        List<Integer> List = new LinkedList<>();
         System.out.printf("%15s %15s %20s %5s\n\n","Method","Type of List","Num of operations","Time(ms)");
         
-        //Добавление
+        //Добавление в LinkedList
         long startTime = System.currentTimeMillis();
         for (int i =0;i<N;i++){
-            linkedList.add(i*i);
+            List.add(random.nextInt());
         }
         long endTime =System.currentTimeMillis();
         long dur=endTime-startTime;
         System.out.printf("%15s %15s %15s %10s\n\n","add","LinkedList",N,dur);
         
+        //Извлечение из LinkedList
         startTime = System.currentTimeMillis();
         for (int i =0;i<N;i++){
-            arrayList.add(i*i);
-        }
-        endTime =System.currentTimeMillis();
-        dur=endTime-startTime;
-        System.out.printf("%15s %15s %15s %10s\n\n","add","ArrayList",N,dur);
-        
-        //Извлечение
-        int value;
-        startTime = System.currentTimeMillis();
-        for (int i =0;i<N;i++){
-            value=linkedList.get(i);
+            List.get(random.nextInt(N-1));
         }
         endTime =System.currentTimeMillis();
         dur=endTime-startTime;
         System.out.printf("%15s %15s %15s %10s\n\n","get","LinkedList",N,dur);
-        
-        startTime = System.currentTimeMillis();
-        for (int i =0;i<N;i++){
-            value=arrayList.get(i);
-        }
-        endTime =System.currentTimeMillis();
-        dur=endTime-startTime;
-        System.out.printf("%15s %15s %15s %10s\n\n","get","ArrayList",N,dur);
-        
-        //Добавление в середину
-        startTime = System.currentTimeMillis();
-        for (int i =0;i<N;i++){
-            linkedList.add((N+i)/2,i*i);
-        }
-        endTime =System.currentTimeMillis();
-        dur=endTime-startTime;
-        System.out.printf("%15s %15s %15s %10s\n\n","middle add","LinkedList",N,dur);
-        
-        startTime = System.currentTimeMillis();
-        for (int i =0;i<N;i++){
-            arrayList.add(N/2,i*i);
-        }
-        endTime =System.currentTimeMillis();
-        dur=endTime-startTime;
-        System.out.printf("%15s %15s %15s %10s\n\n","middle add","ArrayList",N,dur);
-        
-        //Удаление
+
+        //Удаление из LinkedList
         startTime = System.currentTimeMillis();
         for (int i =N-1;i>=0;i--){
-            linkedList.remove(i);
+            List.remove(i);
         }
         endTime =System.currentTimeMillis();
         dur=endTime-startTime;
         System.out.printf("%15s %15s %15s %10s\n\n","remove","LinkedList",N,dur);
         
+        List=new ArrayList<>();
+        
+        //Добавление в ArrayList
+        startTime = System.currentTimeMillis();
+        for (int i =0;i<N;i++){
+            List.add(random.nextInt());
+        }
+        endTime =System.currentTimeMillis();
+        dur=endTime-startTime;
+        System.out.printf("%15s %15s %15s %10s\n\n","add","ArrayList",N,dur);
+        
+        //Извлечение из ArrayList
+        startTime = System.currentTimeMillis();
+        for (int i =0;i<N;i++){
+           List.get(random.nextInt(N-1));
+        }
+        endTime =System.currentTimeMillis();
+        dur=endTime-startTime;
+        System.out.printf("%15s %15s %15s %10s\n\n","get","ArrayList",N,dur);
+        
+        //Удаление из ArrayList
         startTime = System.currentTimeMillis();
         for (int i =N-1;i>=0;i--){
-            arrayList.remove(i);
+            List.remove(i);
         }
         endTime =System.currentTimeMillis();
         dur=endTime-startTime;
         System.out.printf("%15s %15s %15s %10s\n\n","remove","ArrayList",N,dur);
-        
 //         Method    Type of List    Num of operations Time(ms)
 //
-//            add      LinkedList           20000          2
+//            add      LinkedList           50000         10
 //
-//            add       ArrayList           20000          2
+//            get      LinkedList           50000        966
 //
-//            get      LinkedList           20000        716
+//         remove      LinkedList           50000          2
 //
-//            get       ArrayList           20000          3
+//            add       ArrayList           50000          2
 //
-//     middle add      LinkedList           20000       1627
+//            get       ArrayList           50000          2
 //
-//     middle add       ArrayList           20000         49
-//
-//         remove      LinkedList           20000       1530
-//
-//         remove       ArrayList           20000         43
+//         remove       ArrayList           50000          2
     }
     
 }
+
